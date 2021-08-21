56fce261f71af11be4796735e1a6c650ec8e1745
bhavya2 <bhavya2@illinois.edu>
2021-08-20 22:43:36 -0500

    doc,med wall, note share txt msg

---
+++ Savvy/Savvy_bridge.h
@@ -0,0 +1,13 @@
+//
+//  Savvy_bridge.h
+//  Savvy
+//
+//  Created by Bhavya on 12/26/20.
+//  Copyright © 2020 uiuc. All rights reserved.
+//
+
+#ifndef Savvy_bridge_h
+#define Savvy_bridge_h
+#include "Mailcore/MailCore.h"
+
+#endif /* Savvy_bridge_h */
